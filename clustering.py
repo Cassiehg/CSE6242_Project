c3b7def8b025cc33b640388c3103d82b53ec3ec8
ckwan48 <ckwan48@gmail.com>
2018-04-04 20:15:12 -0400

    added clustering.py file

---
+++ clustering.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  4 17:08:40 2018
+
+@author: chriskwan
+"""
+import numpy as np
+from sklearn.cluster import KMeans
+
+def fun(filename):
+    with open(filename, 'rb') as f:
+        f = 
+    
+     
+
+    
+    
+    
+if __name__ == '__main__':
+    fun()
